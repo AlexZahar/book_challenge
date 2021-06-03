547fe8fb56c5cf513b7aad4bd41d1cb303c7b3a7
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-06-03 13:51:23 +0200

    Splitted the app in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EditorComponent } from './pages/editor/editor.component';
+import { CollectionComponent } from './pages/collection/collection.component';
+import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+import { ContentHeaderComponent } from './components/content-header/content-header.component';
+import { ContentAreaComponent } from './pages/content-area/content-area.component';
+import { CardComponent } from './components/card/card.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, EditorComponent, CollectionComponent, SideNavigationComponent, ContentHeaderComponent, ContentAreaComponent, CardComponent, SearchBoxComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
