08a5c89c636bfe454bcea10fd31c61f95c62247a
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-06-04 15:26:42 +0200

    Cleaned up the single card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,31 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
-// import { ApiService } from '../../core/services/api.service';
-// import { Book } from '../../core/interfaces/book';
+
+import { Book } from '../../core/interfaces/book';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  // books: Book[] = [];
-  @Input() book: any;
+  @Input() book: Book;
   constructor() {}
-  // book: any;
-  ngOnInit(): void {
-    // this.getBooks();
-  }
 
-  // getBooks() {
-  //   try {
-  //     this.api.getBooks().subscribe(data => {
-  //       console.log('DATAAA', data);
-  //       this.books = data;
-  //       this.book = this.books[1];
-
-  //       console.log(this.book.cover);
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  ngOnInit(): void {}
 }
