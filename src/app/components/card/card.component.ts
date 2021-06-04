bd7f16624a79a97ac143334f601883d71eafa2a7
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-06-04 14:26:57 +0200

    Looping trhough list of books in the collection component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../core/services/api.service';
-import { Book } from '../../core/interfaces/book';
+import { Component, OnInit, Input } from '@angular/core';
+// import { ApiService } from '../../core/services/api.service';
+// import { Book } from '../../core/interfaces/book';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  books: Book[] = [];
-  constructor(private api: ApiService) {}
-  book: any;
+  // books: Book[] = [];
+  @Input() book: any;
+  constructor() {}
+  // book: any;
   ngOnInit(): void {
-    this.getBooks();
+    // this.getBooks();
   }
 
-  getBooks() {
-    try {
-      this.api.getBooks().subscribe(data => {
-        console.log('DATAAA', data);
-        this.books = data;
-        this.book = this.books[1];
+  // getBooks() {
+  //   try {
+  //     this.api.getBooks().subscribe(data => {
+  //       console.log('DATAAA', data);
+  //       this.books = data;
+  //       this.book = this.books[1];
 
-        console.log(this.book.cover);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  //       console.log(this.book.cover);
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 }
--- src/app/pages/collection/collection.component.ts
+++ src/app/pages/collection/collection.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/core/services/api.service';
+import { Book } from 'src/app/core/interfaces/book';
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
-  styleUrls: ['./collection.component.scss']
+  styleUrls: ['./collection.component.scss'],
 })
 export class CollectionComponent implements OnInit {
+  books: Book[] = [];
+  // book: any;
 
-  constructor() { }
-
+  constructor(private api: ApiService) {}
   ngOnInit(): void {
+    this.getBooks();
   }
 
+  getBooks() {
+    try {
+      this.api.getBooks().subscribe(data => {
+        console.log('DATAAA', data);
+        this.books = data;
+        // this.book = this.books[1];
+
+        // console.log(this.book.cover);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
