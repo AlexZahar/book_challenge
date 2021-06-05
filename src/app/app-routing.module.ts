b59803573434371810aa3c1bc001520c3321fbeb
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-06-05 22:08:23 +0200

    navigation styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CollectionComponent } from './pages/collection/collection.component';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
 
 const routes: Routes = [
   // { path: 'home', component: AppComponent },
 
   { path: 'collection', component: CollectionComponent },
-  // { path: 'editor', component: HomeComponent },
+  { path: 'editor', component: CardComponent },
 ];
 
 @NgModule({
