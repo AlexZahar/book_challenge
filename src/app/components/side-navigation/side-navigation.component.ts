6a6e73df8514b2a6440e308fe42452aa78f28dd7
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-06-05 13:40:22 +0200

    Layout styiling

--- src/app/components/side-navigation/side-navigation.component.ts
+++ src/app/components/side-navigation/side-navigation.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-navigation',
   templateUrl: './side-navigation.component.html',
-  styleUrls: ['./side-navigation.component.scss']
+  styleUrls: ['./side-navigation.component.scss'],
 })
 export class SideNavigationComponent implements OnInit {
+  expanded = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggle() {
+    this.expanded = !this.expanded;
   }
-
 }
